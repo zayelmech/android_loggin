cab3103d95cc15282c6e3308002b164ac942f536
zayelmech <i.equipo001@gmail.com>
2022-05-27 10:46:27 -0400

    new activity was add

--- app/src/main/java/com/enhanceit/loginscreen/MainActivity.java
+++ app/src/main/java/com/enhanceit/loginscreen/MainActivity.java
@@ -2,13 +2,41 @@ package com.enhanceit.loginscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button mButton1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("a", "onCreate: ");
+        mButton1=findViewById(R.id.createaccount);
+        mButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(getBaseContext(),MainActivity2.class);
+                mIntent.putExtra("MY_DATA","WHATEVER");
+
+                startActivity(mIntent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
